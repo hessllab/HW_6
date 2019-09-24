5db3b5a5976d57db8a03f3f04472d61231d564ad
sis0004 <54180343+sis0004@users.noreply.github.com>
2019-09-24 18:27:28 -0400

    Upload updated state.sh

---
+++ state.sh
@@ -0,0 +1,13 @@
+mkdir -p ITRDB_by_state/"$1"
+cd ITRDB_by_state/"$1"
+wget -r -e robots=off -A "$1"'*.rwl' -R "$1"'*-noaa.rwl' -np -nd https://www1.ncdc.noaa.gov/pub/data/paleo/treering/measurements/northamerica/usa/
+
+for sitename in "$1"*.rwl
+do
+  echo $sitename | cut -d '.' -f 1 >> "$1"_sites.txt
+  head -n 1 $sitename >> "$1"_sites.txt
+  echo  >> "$1"_sites.txt
+done
+
+cd ITRDB_by_state/ny
+wc -l $(grep -l "Cook" ny*.rwl) | sort | sed '$d' | cut -c 10- >> CookFiles.txt
