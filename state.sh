920006d7323fdde563a1bb353e71fb07ed180efa
Tobi Ore <54361683+tobi-ore@users.noreply.github.com>
2019-09-25 09:27:14 -0400

    Update state.sh

--- state.sh
+++ state.sh
@@ -1,9 +1,13 @@
-# Extracts the tree_ring data for specified states and save it in a new file
+## This script extracts the tree_ring data for specified states from NOAA's National Climatic Data Center repo and saves it in a new file
 # Usage: bash state.sh state_abbrev
-mkdir "$1" && cd "$1"
+# Products: 'state_abbrev'_sites.txt, 'state_abbrev'/ directory containing rwl files for that state
+
+mkdir "$1" && cd "$1"  
 wget -r -e robots=off -A "$1"*.rwl -np -nd -q -R "*noaa.rwl" https://www1.ncdc.noaa.gov/pub/data/paleo/treering/measurements/northamerica/usa/
+
 for sitename in *.rwl 
     do 
-        echo $sitename | cut -d . -f 1 >> "$1"_name.txt
-        head -n 1 $sitename >> "$1"_name.txt 
+        echo $sitename | cut -d . -f 1 >> "$1"_sites.txt   #writes the name of the .rwl without the extension to a textfile named 'state_abbrev'_sites
+        
+        head -n 1 $sitename >> "$1"_sites.txt #writes the first line of the .rwl file to the textfile
 done
