0a05f4589473a9a30d3468684dd86415147e8850
ahessl <ahessl@users.noreply.github.com>
2019-10-02 16:55:36 -0400

    cd ..

--- state.sh
+++ state.sh
@@ -1,5 +1,6 @@
-mkdir output
+mkdir output 
 cd output
 wget -r -e robots=off -A 'ny???.rwl' -np -nd https://www1.ncdc.noaa.gov/pub/data/paleo/treering/measurements/northamerica/usa/
 
 wc -l $(grep Cook *.rwl | cut -d : -f 1) | sort -rn | tail -n 10 >> CooksFiles.txt
+cd .. #gets you back where you started
